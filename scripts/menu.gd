54e9d54b1e845c9087987b8a59334b0cc5d0a8f3
DarylBarrel1 <dwir0003@student.monash.edu>
2026-09-23 12:51:32 +1000

    Added sound to the menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -9,18 +9,24 @@ const GAME_SCENE = "res://scenes/main.tscn"
 
 func _ready() -> void:
 	_show_main()
-
+	
+	
+func _on_button_hovered() -> void:
+	$Hover.play()
 
 func _on_start_pressed() -> void:
 	$Fade.color.a = 0.0
+	$Click.play(0.1)
 	await create_tween().tween_property($Fade, "color:a", 1.0, 2.5).finished
 	get_tree().change_scene_to_file(GAME_SCENE)
 
 func _on_credits_pressed() -> void:
+	$Click.play(0.1)
 	_show(credits_panel)
 
 
 func _on_back_pressed() -> void:
+	$Click.play(0.1)
 	_show_main()
 
 func _show_main() -> void:
