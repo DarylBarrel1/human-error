f47bd2e7453f59fcf1cb5463da00a7b960181f2c
DarylBarrel1 <dwir0003@student.monash.edu>
2026-09-23 10:21:38 +1000

    Fixed and designed the win and lose scenes

--- scripts/lose.gd
+++ scripts/lose.gd
@@ -4,7 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Buttons/RetryButton.pressed.connect(retry)
-	$Buttons/MenuButton.pressed.connect(return_to_menu)
+	$Buttons/QuitButton.pressed.connect(return_to_menu)
 
 func retry() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
