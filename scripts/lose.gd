1f575a923b495224815d4d42bd9006252c4df6e6
DarylBarrel1 <dwir0003@student.monash.edu>
2026-09-23 13:03:02 +1000

    Finished adding sounds and transitions to lose and win page

--- scripts/lose.gd
+++ scripts/lose.gd
@@ -1,13 +1,19 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Buttons/RetryButton.pressed.connect(retry)
 	$Buttons/QuitButton.pressed.connect(return_to_menu)
 
 func retry() -> void:
+	$Fade.color.a = 0.0
+	$Click.play(0.1)
+	await create_tween().tween_property($Fade, "color:a", 1.0, 0.5).finished
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func return_to_menu() -> void:
+	$Click.play(0.1)
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_button_hovered() -> void:
+	$Hover.play()
--- scripts/win.gd
+++ scripts/win.gd
@@ -6,4 +6,8 @@ func _ready() -> void:
 	$Buttons/QuitButton.pressed.connect(return_to_menu)
 
 func return_to_menu() -> void:
+	$Click.play(0.1)
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_button_hovered() -> void:
+	$Hover.play()
